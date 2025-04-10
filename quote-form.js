c156b57ca9d836f02e9da068f206ae54fbf216bf
Paulenski205 <paul@supplementpartners.com>
2025-04-09 17:41:28 -0700

    Update quote-form.js

--- quote-form.js
+++ quote-form.js
@@ -1,9 +1,11 @@
 class QuoteForm extends HTMLElement {
   connectedCallback() {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "https://raw.githubusercontent.com/Paulenski205/gentry-wix/refs/heads/main/wix-form-styles.css";
-    document.head.appendChild(link);
+if (!document.querySelector('link[href*="wix-form-styles.css"]')) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://cdn.jsdelivr.net/gh/Paulenski205/gentry-wix@refs/heads/main/wix-form-styles.css";
+  document.head.appendChild(link);
+}
 
     this.quoteData = {};
 
